a5ad0df3924e8dc28642760919976230cf5bbe7a
cpatigny <63152989+cpatigny@users.noreply.github.com>
2020-11-24 13:40:24 +0100

    add edit word and show word card

--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const Word = ({ id, word, translation }) => {
+const Word = ({ id, word, translation, lastRepetition, openWordCard }) => {
   return (
-    <div className='word-box'>
+    <div 
+      className='word-box'
+      onClick={() => openWordCard({ id, word, translation, lastRepetition })} 
+    >
       <p className='word'>{ word }</p>
       <div><span>-&#62;</span></div>
       <p className='translation'>{ translation }</p>
