c0eaa7f2dad185118a225f91f84c2a428d2259bc
cpatigny <63152989+cpatigny@users.noreply.github.com>
2020-12-12 18:14:50 +0100

    menu animation with react spring

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useTransition, animated } from 'react-spring';
 
 import './Menu.css';
 
@@ -7,39 +8,68 @@ const Menu = ({ isShow, handleClose, handleSignOut }) => {
   const handleOverlayClick = e => {
     if (e.currentTarget === e.target) handleClose();
   };
-
+  
   useEffect(() => {
-    let newValue = isShow ? 'hidden' : 'visible';
-    document.body.style.overflow = newValue;
+    document.body.style.overflow = isShow ? 'hidden' : 'visible';
   }, [isShow]);
 
-  let menu = null;
+  const config = { tension: 360, friction: 50 };
 
-  if (isShow) {
-    menu = (
-      <>
-        <div onClick={handleOverlayClick} id='menu-overlay'></div>
-        <aside className='menu'>
-          <div className='top'>
-            <button className='close-menu' onClick={handleClose}>
-              <span className='material-icons-round'>close</span>
-            </button>
-            <h1><span>VocabuLearning</span></h1>
-          </div>
-          <nav>
-            <ul>
-              <li tabIndex='0' className='sign-out' onClick={handleSignOut}>
-                <span className='material-icons-round'>exit_to_app</span>
-                Se déconnecter
-              </li>
-            </ul>
-          </nav>
-        </aside>
-      </>
-    );
-  }
+  const overlayTransitions = useTransition(isShow, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: config
+  });
 
-  return menu;
+  const menuTransitions = useTransition(isShow, null, {
+    from: { transform: 'translateX(-100%)' },
+    enter: { transform: 'translateX(0)' },
+    leave: { transform: 'translateX(-100%)' },
+    config: config
+  });
+
+  return (
+    <>
+      {
+        overlayTransitions.map(({ item, key, props }) =>
+          item && 
+          <animated.div
+            key={key}
+            style={props}
+            onClick={handleOverlayClick}
+            id='menu-overlay'
+          />
+        )
+      }
+
+      {
+        menuTransitions.map(({ item, key, props }) =>
+          item && 
+          <animated.aside
+            key={key}
+            style={props}
+            className='menu'
+          >
+            <div className='top'>
+              <button className='close-menu' onClick={handleClose}>
+                <span className='material-icons-round'>close</span>
+              </button>
+              <h1><span>VocabuLearning</span></h1>
+            </div>
+            <nav>
+              <ul>
+                <li tabIndex='0' className='sign-out' onClick={handleSignOut}>
+                  <span className='material-icons-round'>exit_to_app</span>
+                  Se déconnecter
+                </li>
+              </ul>
+            </nav>
+          </animated.aside>
+        )
+      }
+    </>
+  );
 }
 
 export default Menu;
