9f03b11554984efb5b35599a674fa31bf73a3350
cpatigny <63152989+cpatigny@users.noreply.github.com>
2023-02-17 00:05:53 +0100

    trim dashes to avoid slug starting/ending with dash(es)

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -1,17 +1,19 @@
+import trim from "./trim";
+
 /**
- * slugify a string and return it. 
+ * slugify a string and return it.
  * @param {string} str the string to slugify
  */
 export const slugify = str => {
   if (!str) throw new Error('str parameter must not be empty');
   if (typeof str !== 'string') throw new Error('str parameter must be of type string');
 
-  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = trim(str, ' '); // trim spaces
   str = str.toLowerCase();
 
   // remove accents, swap ñ for n, etc
-  const from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
-  const to   = "aaaaeeeeiiiioooouuuunc------";
+  const from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;'";
+  const to   = "aaaaeeeeiiiioooouuuunc-------";
 
   for (let i = 0, l = from.length; i < l; i++) {
     str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
@@ -21,6 +23,8 @@ export const slugify = str => {
     .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
     .replace(/\s+/g, '-') // collapse whitespace and replace by -
     .replace(/-+/g, '-'); // collapse dashes
+
+  str = trim(str, '-');
 
   return str;
 };
