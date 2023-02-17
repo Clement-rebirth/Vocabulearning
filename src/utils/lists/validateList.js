30fa2d7eaefcad04211b88ac65a017ec91a3cc90
cpatigny <63152989+cpatigny@users.noreply.github.com>
2023-02-17 01:36:30 +0100

    added the list id in the slug so that the slug is always unique

--- src/utils/lists/validateList.js
+++ src/utils/lists/validateList.js
@@ -1,18 +1,10 @@
 import containsOnlySpecialChars from "../containsOnlySpecialChars";
-import { slugify } from "../slugify";
 
 export const validateList = (name, existingLists) => {
   let errors = [];
 
   if (containsOnlySpecialChars(name)) errors.push('Le nom doit contenir au moins un chiffre ou une lettre');
-
   if (name.length > 50) errors.push('Le nom ne doit pas dépasser 50 caractères');
-
-  let list = existingLists && Object
-    .keys(existingLists)
-    .find(key => slugify(existingLists[key].name) === slugify(name));
-
-  if (list) errors.push('Une liste existe déjà avec ce nom');
 
   return errors;
 };
